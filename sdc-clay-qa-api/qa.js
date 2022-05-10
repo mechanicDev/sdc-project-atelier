03b88a3dba35253e63b5136e65c8783e2cf0b729
Jarrod <jarrod83miller@gmail.com>
2022-05-10 00:24:41 -0500

    No changes

--- sdc-clay-qa-api/qa.js
+++ sdc-clay-qa-api/qa.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const DB = require('./database/index.js');
-const U = require('./Utils/utils.js');
-const URL = require('url');
+const DB = require("./database/index.js");
+const U = require("./Utils/utils.js");
+const URL = require("url");
 
 router.use((req, res, next) => {
   // console.log('Time: ', Date.now());
@@ -15,21 +15,20 @@ router.use((req, res, next) => {
 
 // Before 170ms after 20 ms
 
-
-router.get('/questions', (req, res) => {
-  console.log('This is the get route for questions/:product_id')
-  url = URL.parse(req.url, true).query
+router.get("/questions", (req, res) => {
+  console.log("This is the get route for questions/:product_id");
+  url = URL.parse(req.url, true).query;
   U.questionsId(url.product_id)
-    .then(data => res.status(200).send(data))
-    .catch(error => res.status(200).send(error));
+    .then((data) => res.status(200).send(data))
+    .catch((error) => res.status(200).send(error));
 });
 
 // Before 300ms after 30ms
-router.get('/questions/:id/answers', (req, res) => {
-  console.log('REquest params, ', req.params)
+router.get("/questions/:id/answers", (req, res) => {
+  console.log("REquest params, ", req.params);
   U.questionsIdAnswers(req.params.id)
-    .then(data => res.status(200).send(data))
-    .catch(error => res.status(400).send(error))
+    .then((data) => res.status(200).send(data))
+    .catch((error) => res.status(400).send(error));
 });
 
 // Test route
@@ -45,39 +44,60 @@ router.get('/questions/:id/answers', (req, res) => {
 //   })
 // })
 
-
 //////////////////
 // Put Requests //
 //////////////////
 
 // Before 30ms, Might not need to do anything with this route
-router.put('/questions/:question_id/helpful', (req, res) => {
+router.put("/questions/:question_id/helpful", (req, res) => {
   U.questionsIdHelpful(req.params.question_id)
-  .then(data => res.status(200).send(`Helpful count has been updated for question Id ${req.params.question_id}`))
-  .catch(data => res.status(400).send(error));
+    .then((data) =>
+      res
+        .status(200)
+        .send(
+          `Helpful count has been updated for question Id ${req.params.question_id}`
+        )
+    )
+    .catch((data) => res.status(400).send(error));
 });
 
 // Before 77ms - 10ms Might not need to index this
-router.put('/answers/:answers_id/helpful', (req, res) => {
+router.put("/answers/:answers_id/helpful", (req, res) => {
   // console.log('Params: ', req.params)
   U.answersIdHelpful(req.params.id)
-  .then(data => res.status(200).send(`Helpful count has been updated for answer ID ${req.params.answer_id}`))
-  .catch(error => res.status(400).send(error));
+    .then((data) =>
+      res
+        .status(200)
+        .send(
+          `Helpful count has been updated for answer ID ${req.params.answer_id}`
+        )
+    )
+    .catch((error) => res.status(400).send(error));
 });
 
 //Before 37 - 12 ms.. no improvement needed?
-router.put('/questions/:question_id/report', (req, res) => {
+router.put("/questions/:question_id/report", (req, res) => {
   // console.log('Params: ', req.params)
   U.questionIdReport(req.params.question_id)
-    .then(data => res.status(200).send(`Question_Id ${req.params.question_id} has been updated`))
-    .catch(error => res.status(401).send(error));
+    .then((data) =>
+      res
+        .status(200)
+        .send(`Question_Id ${req.params.question_id} has been updated`)
+    )
+    .catch((error) => res.status(401).send(error));
 });
 
 //Before 16-11 ms. No improvements needed?
-router.put('/answers/:answer_id/report', (req, res) => {
+router.put("/answers/:answer_id/report", (req, res) => {
   U.answersIdReported(req.params.answer_id)
-    .then(data => res.status(200).send(`Reported count has been updated for answer ID ${req.params.answer_id}`))
-    .catch(error => res.status(400).send(error));
+    .then((data) =>
+      res
+        .status(200)
+        .send(
+          `Reported count has been updated for answer ID ${req.params.answer_id}`
+        )
+    )
+    .catch((error) => res.status(400).send(error));
 });
 
 ///////////////////
@@ -85,18 +105,17 @@ router.put('/answers/:answer_id/report', (req, res) => {
 ///////////////////
 
 // 21ms -
-router.post('/questions', (req, res) => {
-  url = URL.parse(req.url, true).query
+router.post("/questions", (req, res) => {
+  url = URL.parse(req.url, true).query;
   let body = url.body;
   let name = url.name;
   let email = url.email;
   let product_id = url.product_id;
-  let date = Date.now()
+  let date = Date.now();
 
   U.questionsPost(body, name, email, product_id, date)
-    .then(data => res.status(200).send('It worked'))
-    .catch(error => res.status(400).send(error));
-
+    .then((data) => res.status(200).send("It worked"))
+    .catch((error) => res.status(400).send(error));
 });
 
 module.exports = router;
